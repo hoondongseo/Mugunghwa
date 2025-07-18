bf9496dbedacf1ffb750e7f2b3c64df1c82f0efc
Donghoon Seo <naosdh@naver.com>
2025-07-18 14:02:52 +0900

    feat: 'sanitize-html’ 패키지와 타입 정의를 설치

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,12 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
-import runtimeErrorOverlay from "@replit/vite-plugin-runtime-error-modal";
 
 export default defineConfig({
 	// 상대 경로로 자산 로드 (파비콘 등) 설정
 	base: process.env.NODE_ENV === "production" ? "./" : "/",
 	plugins: [
 		react(),
-		runtimeErrorOverlay(),
 		...(process.env.NODE_ENV !== "production" &&
 		process.env.REPL_ID !== undefined
 			? [
